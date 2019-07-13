1ce6e29dcf49d19a207354ad6eee3e75669e282b
Bach Ngoc Toan <ngoctoan@live.com>
2019-07-13 22:11:58 +0700

    Add MVC

---
+++ MVCCoreApp/MVCCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new IndexModel();
+            model.Message = "Hello from Model";
+            return View(model);
+        }
+    }
+}
---
+++ MVCCoreApp/MVCCoreApp/Models/IndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreApp.Models
+{
+    public class IndexModel
+    {
+        public string Message { set; get; }
+    }
+}
