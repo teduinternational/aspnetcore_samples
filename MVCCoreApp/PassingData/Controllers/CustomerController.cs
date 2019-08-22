c2155961055191a4e2db9ed6485e06c29bb9088f
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-22 08:50:36 +0700

    View

---
+++ MVCCoreApp/PassingData/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PassingData.Models;
+
+namespace PassingData.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewBag.Customer = new Customer();
+            //ViewData.Model = new Customer();
+            var customer = new Customer();
+            return View(customer);
+        }
+    }
+}
---
+++ MVCCoreApp/PassingData/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassingData.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Customer()
+        {
+            CustomerID = 1;
+            Name = "Rahul Dravid";
+            Address = "Bangalore";
+        }
+
+    }
+}
