e15fccaaa102eb89627952d83492a3669eb33397
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-22 09:03:14 +0700

    From

---
+++ MVCCoreApp/ASPNetCoreForms/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNetCoreForms.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductEditModel model)
+        {
+            string message = string.Empty;
+            if (ModelState.IsValid)
+            {
+                message = "Product: " + model.Name + ". Rate: "
+                          + model.Rate + ". Rating: " + model.Rating + " created successfully";
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again.";
+            }
+            return Content(message);
+        }
+    }
+}
---
+++ MVCCoreApp/ASPNetCoreForms/Models/ProductEditModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreForms.Models
+{
+    public class ProductEditModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+        public int Rating { get; set; }
+    }
+}
