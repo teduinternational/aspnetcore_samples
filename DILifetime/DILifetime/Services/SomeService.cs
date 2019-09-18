1f1859aed7089f41035e470245a221ca44d73b1e
Bach Ngoc Toan <ngoctoan@live.com>
2019-09-18 22:45:40 +0700

    DI Lifetime

---
+++ DILifetime/DILifetime/Services/SomeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DILifetime.Services
+{
+    public class SomeService : ITransientService, IScopedService, ISingletonService
+    {
+        Guid _id;
+
+        public SomeService()
+        {
+            _id = Guid.NewGuid();
+        }
+        public Guid GetID()
+        {
+            return _id;
+        }
+    }
+}
